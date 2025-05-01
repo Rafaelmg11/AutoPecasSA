93bad9272d587d8dcd3213d5474280ec00fb7347
Rafaelmg11 <almeidarafaelmagalhaes@gmail.com>
2025-05-01 20:13:53 -0300

    Projeto Teste

---
+++ Crud_novo.py
@@ -0,0 +1,50 @@
+import mysql.connector
+from config import MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE
+
+#pip install mysql-connector-python
+
+def get_connection():
+    return mysql.connector.connect(
+        host = MYSQL_HOST,
+        user = MYSQL_USER,
+        password = MYSQL_PASSWORD,
+        database = MYSQL_DATABASE,
+    )
+
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+#FUNCÕES PECA:
+
+
+#COMBO BOX:
+def selecionar_fornecedores():
+
+    conn = get_connection()
+    cursor = conn.cursor()
+    query = "SELECT cod_fornec,nome_fornec FROM fornecedor ORDER BY nome_fornec ASC"
+    cursor.execute(query)
+    fornecedores = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return fornecedores
+
+def obter_cod_fornecedor(nome_fornecedor):
+    conn = get_connection()
+    cursor = conn.cursor()
+    query = ("SELECT cod_fornec FROM fornecedor WHERE nome_fornec = %s")
+    cursor.execute(query,(nome_fornecedor,))
+    resultado = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    return resultado[0] if resultado else None
+
+def selecionar_tipopeca():
+    conn = get_connection()
+    cursor = conn.cursor()
+    query = "SELECT tipo_peca FROM peca ORDER BY tipo_peca ASC"
+    cursor.execute(query)
+    tipo_peca = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return tipo_peca
+
---
+++ Tela_Peça.py
@@ -0,0 +1,156 @@
+import customtkinter as ctk
+import mysql.connector
+from tkinter import messagebox,filedialog #filedialog abre janelas de seleção de arquivos
+from tkinter import ttk
+from PIL import Image, ImageTk #Image:abrir,redimensionar e manipular, ImageTk: converter em widgets para exibição 
+import io #Fluxo de bytes (transforma imagem em bytes)
+from Crud_novo import selecionar_fornecedores,selecionar_tipopeca,obter_cod_fornecedor
+
+app = ctk.CTk()   
+
+app.title("CADASTRO DE PEÇAS") #Titulo
+app.geometry("700x680") #Tamanho da janela
+app.configure(fg_color = "#5424A2") #Cor de fundo da janela
+app.resizable(width = False,height = False) #Impede que a janela seja redimensionada 
+
+#Declarando variaveis futuras:
+cod_fornecedor_selecionado = None
+imagem_padrao_pil = Image.open("sem_imagem.png").resize((120,120)) #Puxa imagem
+imagem_padrao = ImageTk.PhotoImage(imagem_padrao_pil) #Converte imagem 
+
+#Imagem atual em bytes
+imagem_bytes = None
+
+
+#Conexão com banco de dados
+def conectarBanco(): #CONEXÃO COM O BANCO DE DADOS
+    conn = mysql.connector.connect(
+        host = "localhost",
+         user = "root",
+        password = "",
+        database = "autopecassa_db"
+    )
+    cursor = conn.cursor()
+
+#FUNÇÕES QUE SELECIONAM X ITEM DA CAMBO BOX (NÃO NECESSARIO(EM TEORIA))
+def selecionado_TipoDePeca(event): #FUNÇÃO QUE PREENCHE A CAMBO BOX
+    selecionado = TipoDePecaCB.get() #VARIAVEL RECEBENDO O VALOR DA COMBO BOX
+    print("Selecionado {}".format(selecionado)) #PRINT DE CONFIRMAÇÃO APENAS
+
+
+def selecionado_fornec(event): #FUNÇÃO QUE PREENCHE A CAMBO BOX
+    nome_selecionado = fornecedorCB.get() #VARIAVEL RECEBENDO O VALOR DA COMBO BOX
+    print("Selecionado {}".format(nome_selecionado)) #PRINT DE CONFIRMAÇÃO APENAS
+
+    cod_fornecedor_selecionado = obter_cod_fornecedor(nome_selecionado) #VARIAVEL RECEBENDO FUNÇÃO DO CRUD DE PEGAR O COD DE FORNECEDOR
+    print(f"Fornecedor selecionado: {nome_selecionado} (Código: {cod_fornecedor_selecionado})") #PRINT DE CONFIRMAÇÃO APENAS
+
+
+#FILTRO DE CAMBO BOXS:
+def filtrar_fornecedores(event): #FUNÇÃO DE FILTRO NA CAMBO BOX
+    texto = fornecedorCB.get().lower() #TEXTO DIGITADO
+    if texto == '':
+        opcoes = nome_fornecedoresLista #MONSTRA TODOS OS FORNECEDORES DA LISTA
+    else:
+        opcoes = [item for item in nome_fornecedoresLista if texto in item.lower()] #FILTRO
+    fornecedorCB['values'] = opcoes #COMBO BOX RECEBENDO OS VALORES DA LISTA "OPÇOES"
+
+
+def filtrar_tipopeca(event):
+    texto = TipoDePecaCB.get().lower() #TEXTO DIGITADO
+    if texto == '':
+        opcoes = TipoPecaLista #MONSTRA TODOS OS TIPODES DE PEÇAS
+    else:
+        opcoes = [item for item in TipoPecaLista if texto in item.lower()] #FILTRO
+    TipoDePecaCB['values'] = opcoes #COMBO BOX RECEBENDO OS VALORES DA LISTA "OPÇOES"
+
+        
+
+
+#CRIANDO COMBO BOXS:
+        
+TipoDePecaTB = selecionar_tipopeca() #RECEBENDO FUNÇÃO DO CRUD DE BUSCAR TODOS OS TIPOS DE PEÇA
+TipoPecaLista = [TipoDePeca[0] for TipoDePeca in TipoDePecaTB] #LISTA
+TipoDePecaCB = ttk.Combobox (values= TipoPecaLista, height=44, width=44, state="normal") #CRIANDO COMBO BOX
+TipoDePecaCB.place(x=180,y=105) #POSICIONANDO COMBO BOX
+TipoDePecaCB.set("Selicione Um Tipo") #FRASE DO FRONT END INICIAL
+TipoDePecaCB.bind("<<ComboboxSelected>>", selecionado_TipoDePeca) #AÇÃO DE SELECIONAR
+TipoDePecaCB.bind("<KeyRelease>",filtrar_tipopeca) #CHAMA A FUNÇÃO DE FILTRO
+
+
+fornecedoresTB = selecionar_fornecedores() #RECEBENDO FUNÇÃO DO CRUD DE BUSCAR TODOS OS FORNECEDORES
+nome_fornecedoresLista = [fornecedor[1] for fornecedor in fornecedoresTB] #LISTA
+fornecedorCB = ttk.Combobox(values = nome_fornecedoresLista,height=44,width=44, state="normal")#CRIANDO COMBO BOX
+fornecedorCB.place(x=166, y= 260) #POSICIONANDO COMBO BOX
+fornecedorCB.set("Selecione um Fornecedor") #FRASE DO FRONT END INICIAL
+fornecedorCB.bind("<<ComboboxSelected>>", selecionado_fornec) #AÇÃO DE SELECIONAR
+fornecedorCB.bind("<KeyRelease>",filtrar_fornecedores) #CHAMA A FUNÇÃO DO FILTRO 
+
+
+
+#CRIANDO LabelS:
+TituloLabel =ctk.CTkLabel(master=app,text="PEÇAS: ",font=("Georgia",25),fg_color = "#5424A2",text_color = "WHITE") 
+TipoDePecaLabel =ctk.CTkLabel(master=app,text = "Tipo de Peça: ",font = ("Georgia",16),fg_color = "#5424A2", text_color = "WHITE") 
+DescricaoLabel =ctk.CTkLabel(master=app,text= "Descrição: ",font= ("Georgia",16),fg_color = "#5424A2", text_color = "WHITE")
+QuantidadeLabel =ctk.CTkLabel (master=app,text= "Quantidade: ",font = ("Georgia",16),fg_color = "#5424A2", text_color = "WHITE") 
+LoteLabel =ctk.CTkLabel(master=app,text="Lote: ",font=("Georgia",16),fg_color = "#5424A2", text_color = "WHITE") 
+ValorLabel =ctk.CTkLabel (master=app,text="Valor: ",font=("Georgia",16),fg_color = "#5424A2", text_color = "WHITE") 
+FornecedorLabel =ctk.CTkLabel (master=app,text="Fornecedor: ",font = ("Georgia",16),fg_color = "#5424A2", text_color = "WHITE")
+CodigoLabel =ctk.CTkLabel (master=app,text="Codigo de Peça: ",font = ("Georgia",16),fg_color = "#5424A2", text_color = "WHITE")
+
+#POSICIONANDO LabelS:
+TituloLabel.pack(pady=40,anchor="center") #POSICIONANDO TITULO
+
+TipoDePecaLabel.place(x=40,y=105)
+DescricaoLabel.place(x=40,y=135)
+QuantidadeLabel.place(x=40,y=165)
+LoteLabel.place(x=40,y=195)
+ValorLabel.place(x=40,y=225)
+FornecedorLabel.place(x=40,y=255)
+CodigoLabel.place(x=40,y=285)
+
+#CRIANDO CAMPOS DE ENTRADAS:
+DescricaoEntry = ctk.CTkEntry(master=app,width=48,font=("Georgia",12))
+QuantidadeEntry = ctk.CTkEntry(master=app,width=14,font=("Georgia",12))
+LoteEntry = ctk.CTkEntry(master=app,width=14,font=("Georgia",12))
+ValorEntry = ctk.CTkEntry(master=app,width=14,font=("Georgia",12))
+CodigoEntry = ctk.CTkEntry(master=app,width=10,font=("Georgia",12))
+PesquisaEntry = ctk.CTkEntry(master=app,width=53,font= ("Georgia",13))
+
+#POSICIONA OS CAMPOS DE ENTRADAS:
+DescricaoEntry.place(x=151, y= 140)
+QuantidadeEntry.place(x=166, y= 170)
+LoteEntry.place(x=214, y= 200)
+ValorEntry.place(x=199, y= 230)
+CodigoEntry.place(x=230,y=290)
+PesquisaEntry.place(x=143,y=392)
+
+#FUNÇÃO PARA CARREGAR IMAGEM:
+def carregar_imagem():
+    global imagem_bytes, imagem_display #Variaveis globais
+    caminho = filedialog.askopenfilename(filetypes=[("Imagens","*.png;*.jpg;*.jpeg")]) # Abre gerenciador de arquivos na pasta "Imagens"(recebe o caminho do arquivo)
+    if caminho:
+        with open(caminho,"rb") as f: #Abre o arquivo localizado em modo de leitura binaria(bytes)
+            imagem_bytes = f.read() #Recebe a leitura e fecha o arquivo
+
+        imagem_pil = Image.open(io.BytesIO(imagem_bytes)) #Transforma os bytes num objeto de manipulação do pyhton e abre a imagem
+        imagem_pil = imagem_pil.resize((120,120)) #Redimenziona a imagem
+        imagem_display = ImageTk.PhotoImage(imagem_pil) #Converte em widget
+        imagem_label.configure(image = imagem_display,text="") #Exibe
+    else:
+        messagebox.showwarning("Atenção","Imagem não selecionada")
+    
+def cadastrar_peca():
+    #OBTENDO AS INFORMAÇÕES DOS CAMPOS DE TEXTOS
+    codigo_fornecedor = cod_fornecedor_selecionado
+    tipoDePeca = TipoDePecaCB.get()
+    descricao = DescricaoEntry.get()
+    quantidade = QuantidadeEntry.get()
+    lote = LoteEntry.get()
+    valor = ValorEntry.get()
+    fornecedor = fornecedorCB.get()
+
+app.mainloop()
+    
+
+
