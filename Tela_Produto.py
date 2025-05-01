270d19439d7be7c17ab1821caf675aad30d5630b
Rafaelmg11 <almeidarafaelmagalhaes@gmail.com>
2025-05-01 12:12:07 -0300

    Update geral em produtos
    Botão de Alterar
    Comentarios e correções simples

--- Tela_Produto.py
+++ Tela_Produto.py
@@ -7,7 +7,7 @@ import tkinter as tk
 import mysql.connector
 
 
-class PRODUTO:  
+class PECA:  
 
     def __init__(self,root): #PARA EXECUTAR ESSE CODIGO SEPAPARADEMENTE DEVE TIRAR O "main_window"
         self.root = root
@@ -23,7 +23,7 @@ class PRODUTO:
         self.create_widgets()
 
 
-    def conectarBanco(self):
+    def conectarBanco(self): #CONEXÃO COM O BANCO DE DADOS
         self.conn = mysql.connector.connect(
             host = "localhost",
             user = "root",
@@ -33,32 +33,35 @@ class PRODUTO:
         self.cursor = self.conn.cursor()
 
 
-    def selecionado_TipoDePeca(self, event):
-        selecionado = self.TipoDePecaCB.get()
-        print("Selecionado {}".format(selecionado))
+    #FUNÇÕES QUE SELECIONAM X ITEM DA CAMBO BOX (NÃO NECESSARIO(EM TEORIA))
+
+    def selecionado_TipoDePeca(self, event): #FUNÇÃO QUE PREENCHE A CAMBO BOX
+        selecionado = self.TipoDePecaCB.get() #VARIAVEL RECEBENDO O VALOR DA COMBO BOX
+        print("Selecionado {}".format(selecionado)) #PRINT DE CONFIRMAÇÃO APENAS
     
 
-    def selecionado_fornec(self, event):
-        nome_selecionado = self.fornecedorCB.get()
-        print("Selecionado {}".format(nome_selecionado))
+    def selecionado_fornec(self, event): #FUNÇÃO QUE PREENCHE A CAMBO BOX
+        nome_selecionado = self.fornecedorCB.get() #VARIAVEL RECEBENDO O VALOR DA COMBO BOX
+        print("Selecionado {}".format(nome_selecionado)) #PRINT DE CONFIRMAÇÃO APENAS
 
-        self.cod_fornecedor_selecionado = obter_cod_fornecedor(nome_selecionado)
-
-        
-        print(f"Fornecedor selecionado: {nome_selecionado} (Código: {self.cod_fornecedor_selecionado})")
+        self.cod_fornecedor_selecionado = obter_cod_fornecedor(nome_selecionado) #VARIAVEL RECEBENDO FUNÇÃO DO CRUD DE PEGAR O COD DE FORNECEDOR
+        print(f"Fornecedor selecionado: {nome_selecionado} (Código: {self.cod_fornecedor_selecionado})") #PRINT DE CONFIRMAÇÃO APENAS
 
     
 
-    def create_widgets(self):
+    def create_widgets(self): 
 
-        def filtrar_fornecedores(event):
+
+        #FILTRO DE CAMBO BOXS:
+
+        def filtrar_fornecedores(event): #FUNÇÃO DE FILTRO NA CAMBO BOX
             texto = self.fornecedorCB.get().lower() #TEXTO DIGITADO
             if texto == '':
                 opcoes = nome_fornecedoresLista #MONSTRA TODOS OS FORNECEDORES DA LISTA
             else:
                 opcoes = [item for item in nome_fornecedoresLista if texto in item.lower()] #FILTRO
-            
-            self.fornecedorCB['values'] = opcoes
+            self.fornecedorCB['values'] = opcoes #COMBO BOX RECEBENDO OS VALORES DA LISTA "OPÇOES"
+
 
         def filtrar_tipopeca(event):
             texto = self.TipoDePecaCB.get().lower() #TEXTO DIGITADO
@@ -66,45 +69,42 @@ class PRODUTO:
                 opcoes = TipoPecaLista #MONSTRA TODOS OS TIPODES DE PEÇAS
             else:
                 opcoes = [item for item in TipoPecaLista if texto in item.lower()] #FILTRO
-            self.TipoDePecaCB['values'] = opcoes
+            self.TipoDePecaCB['values'] = opcoes #COMBO BOX RECEBENDO OS VALORES DA LISTA "OPÇOES"
 
-         #CRIANDO COMBO BOX:
+
+        #CRIANDO COMBO BOXS:
         
-        TipoDePecaTB = selecionar_tipopeca()
-        TipoPecaLista = [TipoDePeca[0] for TipoDePeca in TipoDePecaTB]
-
-        self.TipoDePecaCB = ttk.Combobox (self.root,values= TipoPecaLista, height=44, width=44, state="normal")
-        self.TipoDePecaCB.place(x=180,y=105)
-        self.TipoDePecaCB.set("Selicione Um Tipo")
-        self.TipoDePecaCB.bind("<<ComboboxSelected>>", self.selecionado_TipoDePeca)
+        TipoDePecaTB = selecionar_tipopeca() #RECEBENDO FUNÇÃO DO CRUD DE BUSCAR TODOS OS TIPOS DE PEÇA
+        TipoPecaLista = [TipoDePeca[0] for TipoDePeca in TipoDePecaTB] #LISTA
+        self.TipoDePecaCB = ttk.Combobox (self.root,values= TipoPecaLista, height=44, width=44, state="normal") #CRIANDO COMBO BOX
+        self.TipoDePecaCB.place(x=180,y=105) #POSICIONANDO COMBO BOX
+        self.TipoDePecaCB.set("Selicione Um Tipo") #FRASE DO FRONT END INICIAL
+        self.TipoDePecaCB.bind("<<ComboboxSelected>>", self.selecionado_TipoDePeca) #AÇÃO DE SELECIONAR
         self.TipoDePecaCB.bind("<KeyRelease>",filtrar_tipopeca) #CHAMA A FUNÇÃO DE FILTRO
 
 
-        fornecedoresTB = selecionar_fornecedores()
-        nome_fornecedoresLista = [fornecedor[1] for fornecedor in fornecedoresTB]
-
-        self.fornecedorCB = ttk.Combobox(self.root,values = nome_fornecedoresLista,height=44,width=44, state="normal")
-        self.fornecedorCB.place(x=166, y= 260)
-        self.fornecedorCB.set("Selecione um Fornecedor")
-        self.fornecedorCB.bind("<<ComboboxSelected>>", self.selecionado_fornec)
+        fornecedoresTB = selecionar_fornecedores() #RECEBENDO FUNÇÃO DO CRUD DE BUSCAR TODOS OS FORNECEDORES
+        nome_fornecedoresLista = [fornecedor[1] for fornecedor in fornecedoresTB] #LISTA
+        self.fornecedorCB = ttk.Combobox(self.root,values = nome_fornecedoresLista,height=44,width=44, state="normal")#CRIANDO COMBO BOX
+        self.fornecedorCB.place(x=166, y= 260) #POSICIONANDO COMBO BOX
+        self.fornecedorCB.set("Selecione um Fornecedor") #FRASE DO FRONT END INICIAL
+        self.fornecedorCB.bind("<<ComboboxSelected>>", self.selecionado_fornec) #AÇÃO DE SELECIONAR
         self.fornecedorCB.bind("<KeyRelease>",filtrar_fornecedores) #CHAMA A FUNÇÃO DO FILTRO 
 
 
-        
-
 
         #CRIANDO LABELS:
-        TituloLabel = Label(self.root,text="PEÇAS: ",font=("Georgia",25),bg = "#5424A2",fg = "WHITE") #Cria Label TITULO
-        TipoDePecaLabel = Label(self.root,text = "Tipo de Peça: ",font = ("Georgia",16),bg = "#5424A2", fg = "WHITE") #Cria Label Peça
-        DescricaoLabel = Label(self.root,text= "Descrição: ",font= ("Georgia",16),bg = "#5424A2", fg = "WHITE")#Cria Label Descrição
-        QuantidadeLabel = Label (self.root,text= "Quantidade: ",font = ("Georgia",16),bg = "#5424A2", fg = "WHITE") #Cria Label Quantidade
-        LoteLabel = Label(self.root,text="Lote: ",font=("Georgia",16),bg = "#5424A2", fg = "WHITE") #Cria Label Lote
-        ValorLabel = Label (self.root,text="Valor: ",font=("Georgia",16),bg = "#5424A2", fg = "WHITE") #Cria Label Valor
-        FornecedorLabel = Label (self.root,text="Fornecedor: ",font = ("Georgia",16),bg = "#5424A2", fg = "WHITE") #Cria Label Fornecedor
-        CodigoLabel = Label (self.root,text="Codigo de Peça: ",font = ("Georgia",16),bg = "#5424A2", fg = "WHITE") #Cria Label Codigo de Peça
+        TituloLabel = Label(self.root,text="PEÇAS: ",font=("Georgia",25),bg = "#5424A2",fg = "WHITE") 
+        TipoDePecaLabel = Label(self.root,text = "Tipo de Peça: ",font = ("Georgia",16),bg = "#5424A2", fg = "WHITE") 
+        DescricaoLabel = Label(self.root,text= "Descrição: ",font= ("Georgia",16),bg = "#5424A2", fg = "WHITE")
+        QuantidadeLabel = Label (self.root,text= "Quantidade: ",font = ("Georgia",16),bg = "#5424A2", fg = "WHITE") 
+        LoteLabel = Label(self.root,text="Lote: ",font=("Georgia",16),bg = "#5424A2", fg = "WHITE") 
+        ValorLabel = Label (self.root,text="Valor: ",font=("Georgia",16),bg = "#5424A2", fg = "WHITE") 
+        FornecedorLabel = Label (self.root,text="Fornecedor: ",font = ("Georgia",16),bg = "#5424A2", fg = "WHITE")
+        CodigoLabel = Label (self.root,text="Codigo de Peça: ",font = ("Georgia",16),bg = "#5424A2", fg = "WHITE")
 
         #POSICIONANDO LABELS:
-        TituloLabel.pack(pady=40,anchor="center") #POSICIONA O TITULO
+        TituloLabel.pack(pady=40,anchor="center") #POSICIONANDO TITULO
 
         TipoDePecaLabel.place(x=40,y=105)
         DescricaoLabel.place(x=40,y=135)
@@ -140,16 +140,15 @@ class PRODUTO:
             # Fechar a janela atual de cadastro de peças e voltar para a janela principal
             self.root.quit()  # Fecha a janela de cadastro de peças (destrói a instância)
             self.root.destroy()  # Fecha a janela de cadastro de peças, liberando recursos
-
             self.main_window.deiconify()  # Reexibe a janela principal
 
-        voltar_button = tk.Button(self.root, text="VOLTAR", width=11, font=("Georgia", 10), command=voltar_para_principal)
+
+        voltar_button = tk.Button(self.root, text="VOLTAR", width=11, font=("Georgia", 10), command=voltar_para_principal) #AÇÃO PARA O BOTÃO
         voltar_button.place(x=20, y=645)
 
        
 
     #FUNÇÃO PRA REGISTRAR NO BANCO DE DADOS:
-
 
         def cadastrarPeca():
             #OBTENDO AS INFORMAÇÕES DOS CAMPOS DE TEXTOS
@@ -161,10 +160,11 @@ class PRODUTO:
             valor = self.ValorEntry.get()
             fornecedor = self.fornecedorCB.get()
 
-            #VERIFICANDO SE TODOS OS CAMPOS ESTÂO PREENCHIDOS:
+            #VERIFICANDO SE TODOS OS CAMPOS ESTÃO PREENCHIDOS:
             if tipoDePeca and descricao and quantidade and lote and valor and fornecedor and codigo_fornecedor:
                 create_peca(tipoDePeca,descricao,quantidade,lote,valor,fornecedor,codigo_fornecedor)
-            #Limpar campos:
+
+            #LIMPAR CAMPOS:
                 self.TipoDePecaCB.set("Selicione Um Tipo")
                 self.DescricaoEntry.delete(0, tk.END)
                 self.QuantidadeEntry.delete(0, tk.END)
@@ -174,29 +174,31 @@ class PRODUTO:
                 self.fornecedorCB.set("Selecione um Fornecedor")
                 self.PesquisaEntry.delete(0, tk.END)
 
-                messagebox.showinfo("Success","Produto criado com sucesso!")
+                messagebox.showinfo("Success","Peça criado com sucesso!")
             else:
                 messagebox.showerror("Error","Todos os campos são obrigatórios" )
 
-        #BOTÃO DE PRODUTO
+        #BOTÃO DE CADASTRO
         CadastrarButton = tk.Button (self.root,text = "CADASTRAR",font= ("Georgia",10),width=13,command=cadastrarPeca)
         CadastrarButton.place(x=40,y=335)
 
-        #LISTAR PRODUTO
-        def listar_produto():
+
+        #LISTAR PEÇA
+        def listar_peca():
             peca = read_peca() #PUXANDO FUNÇÃO DO CRUD
             self.text_area.delete(1.0, tk.END) #ACESSANDO A "LISTA" DA TELA
-            for peca in peca: #produto ANDANDO EM produtos
+            for peca in peca: #peça ANDANDO EM peças
                 self.text_area.insert(tk.END, f"COD.Peça: {peca[0]}, Tipo de Peça: {peca[1]}, Descricao: {peca[2]},Quantidade: {peca[3]},Lote: {peca[4]},Valor Unitario: {peca[5]},Fornecedor: {peca[6]}\n")
     
         #BOTÃO DE LISTAR:
-        ListarButton = tk.Button (self.root,text="LISTAR",font= ("Georgia",10),width=13,command=listar_produto)
+        ListarButton = tk.Button (self.root,text="LISTAR",font= ("Georgia",10),width=13,command=listar_peca)
         ListarButton.place(x=290,y=335)
-        #FUNÇÃO DE ALTERAR PRODUTO:
-        def alterar_produto():
+
+
+        #FUNÇÃO DE ALTERAR PEÇA:
+        def alterar_peca():
                 
                 #RECEBENDO VALORES
-                
                 codigo_fornecedor = self.cod_fornecedor_selecionado
                 tipoDePeca = self.TipoDePecaCB.get()
                 descricao = self.DescricaoEntry.get()
@@ -204,20 +206,23 @@ class PRODUTO:
                 lote = self.LoteEntry.get()
                 valor = self.ValorEntry.get()
                 fornecedor = self.fornecedorCB.get()
-                cod_peca = self.CodigoEntry.get() #RECEBENDO O VALOR QUE É PRA SER O CODPRODUTO DA TABELA
+                nome_selecionado = self.fornecedorCB.get() #VARIAVEL RECEBENDO O NOME DO FORNECEDOR(PARA OBTER O CODIGO FORNECEDOR)
+                self.cod_fornecedor_selecionado = obter_cod_fornecedor(nome_selecionado) #VARIAVEL RECEBENDO O CODIGO DO FORNECEDOR
+                codigo_fornecedor = self.cod_fornecedor_selecionado #VARIAVEL FINAL
+                cod_peca = self.CodigoEntry.get() #RECEBENDO O VALOR QUE É PRA SER O CODPECA DA TABELA
 
+                #CONEXÃO COM O BANCO DE DADOS
                 conn = get_connection() #VARIAVEL PARA RECEBER A CONEXÃO
-                self.cursor = conn.cursor() #sell.conn TRABALHAR COM A CONEXAO
-
+                self.cursor = conn.cursor() #self.conn TRABALHAR COM A CONEXAO
                 try:
-                    self.cursor.execute("SELECT * FROM peca WHERE cod_peca=%s ",(cod_peca,)) 
                     # CONSULTA NO BANCO
+                    self.cursor.execute("SELECT * FROM peca WHERE cod_peca=%s ",(cod_peca,))  
                     peca_pesquisa = self.cursor.fetchone()
         
-                    # Verificando se o produto foi encontrado
+                    # Verificando se o peça foi encontrado
                     if peca_pesquisa:  # SE FOI ENCONTRADO...
-                        if cod_peca and tipoDePeca and descricao and quantidade and lote and valor and fornecedor:
-                            update_peca(tipoDePeca,descricao,quantidade,lote,valor,fornecedor,cod_peca,codigo_fornecedor) #PUXANDO A FUNÇÃO DO CRUD E AS VARIAVEIS
+                        if cod_peca and tipoDePeca and descricao and quantidade and lote and valor and fornecedor and codigo_fornecedor : #SE TODAS A VARIAVEIS FORAM PREENCHIDAS...
+                            update_peca(tipoDePeca,descricao,quantidade,lote,valor,fornecedor,cod_peca,codigo_fornecedor) #PUXANDO A FUNÇÃO DO CRUD E PASSANDO AS VARIAVEIS
 
                             #LIMPAR CAMPOS
                             self.TipoDePecaCB.set("Selicione Um Tipo")
@@ -229,19 +234,18 @@ class PRODUTO:
                             self.fornecedorCB.set("Selecione um Fornecedor")
                             self.CodigoEntry.delete(0, tk.END)
                             self.PesquisaEntry.delete(0, END)
-                            messagebox.showinfo("Success","Produto alterado com sucesso!")
+                            messagebox.showinfo("Success","Peça alterado com sucesso!")
+
                         else:
                             messagebox.showerror("Error","Todos os campos são obrigatórios")
                     else:
-                        messagebox.showerror("Error","Cadastro de Produto não existe")
+                        messagebox.showerror("Error","Cadastro de Peça não existe")
 
                 except:
                     print("expect")
-
-                #PRECISO PREENCHER O CODIGO FORNECEDOR NA TABELA 
         
         #BOTÃO ALTERAR
-        AlterarButton = tk.Button(self.root,text = "ALTERAR",font= ("Georgia",10),width=13,command=alterar_produto)
+        AlterarButton = tk.Button(self.root,text = "ALTERAR",font= ("Georgia",10),width=13,command=alterar_peca)
         AlterarButton.place(x=164,y=335)  
 
         #FUNÇÃO DE EXCLUIR
@@ -250,14 +254,13 @@ class PRODUTO:
             conn = get_connection() #VARIAVEL PARA RECEBER A CONEXÃO
             self.cursor = conn.cursor() #sell.conn TRABALHAR COM A CONEXAO
             try:
-                self.cursor.execute("SELECT * FROM peca WHERE cod_peca=%s ",(codigo_peca,)) 
-
                 # CONSULTA NO BANCO
+                self.cursor.execute("SELECT * FROM peca WHERE cod_peca=%s ",(codigo_peca,)) 
                 peca_pesquisa = self.cursor.fetchone()
         
-                # Verificando se o produto foi encontrado
+                # Verificando se o peça foi encontrado
                 if peca_pesquisa:  # SE FOI ENCONTRADO...
-                    delete_peca(codigo_peca) #PUXANDO FUNÇÃO DO CRUD
+                    delete_peca(codigo_peca) #PUXANDO FUNÇÃO DO CRUD E PASSANDO A VARIAVEL
 
                     #LIMPAR CAMPOS
                     self.TipoDePecaCB.set("Selicione Um Tipo")
@@ -268,9 +271,9 @@ class PRODUTO:
                     self.CodigoEntry.delete(0, tk.END)
                     self.fornecedorCB.set("Selecione um Fornecedor")
                     self.PesquisaEntry.delete(0, tk.END)
-                    messagebox.showinfo("Success","Produto excluido com sucesso")
+                    messagebox.showinfo("Success","Peça excluido com sucesso")
                 else:
-                    messagebox.showerror("Error","Codigo de Produto não existe")
+                    messagebox.showerror("Error","Codigo de Peça não existe")
             except Exception as e:
                 print(f'Error: {e}') #SE EXEPT, EXIBE O ERRO 
 
@@ -279,22 +282,20 @@ class PRODUTO:
         ExcluirButton.place(x=418,y=335)
   
 
-        #FUNÇÃO DE PESQUISAR :) ;) OBS: NAO TEM RELAÇÃO COM O CRUD
+        #FUNÇÃO DE PESQUISAR OBS: NAO TEM RELAÇÃO COM O CRUD
         def pesquisar_peca():
-            codigo_peca = self.PesquisaEntry.get() 
+            pesquisa = self.PesquisaEntry.get() #RECEBENDO VALOR PARA PESQUISAR
+
             conn = get_connection() #VARIAVEL PARA RECEBER A CONEXÃO
             self.cursor = conn.cursor() #sell.conn TRABALHAR COM A CONEXAO
             try:
-                
-                self.cursor.execute("SELECT tipo_peca, desc_peca, qtde_estoque, lote, valor_unitario, fornecedor, cod_peca FROM peca WHERE cod_peca=%s or desc_peca=%s", (codigo_peca,codigo_peca)) 
-                # ACIMA SELECIONA AS COLUNAS DA TABELA SE codproduto OU produto OU descricao == codigo_produto
-                # codproduto E produto E descricao PERMITE FAZER A BUSCA POR PRODUTO,DESCRICAO, E CODIGO DE PRODUTO
-                #EM OUTROS CASOS PODERIA SER CPF E NÚMERO DE TELEFONE
-
                 # CONSULTA NO BANCO
+                self.cursor.execute("SELECT tipo_peca, desc_peca, qtde_estoque, lote, valor_unitario, fornecedor, cod_peca FROM peca WHERE cod_peca=%s or desc_peca=%s", (pesquisa,pesquisa)) 
+                # ACIMA SELECIONA AS COLUNAS DA TABELA SE codpeca OU descpeca == pesquisa (o que foi digitado no campo de pesquisa)
+                # PERMITE PESQUISA POR DESCRICAO E CODIGO DA PECA
                 peca_pesquisa = self.cursor.fetchone()
         
-                # Verificando se o produto foi encontrado
+                # Verificando se o peça foi encontrado
                 if peca_pesquisa:  # SE FOI ENCONTRADO...
                     tipoDePeca, descricao, quantidade, lote, valor, fornecedor, cod_peca = peca_pesquisa #ESSAS VARIAVEIS VAI RECEBER OS VALORES DA COLUNA DE ACORDO COM A ORDEM
 
@@ -311,25 +312,27 @@ class PRODUTO:
                     self.PesquisaEntry.delete(0, END)
 
                     # Inserindo os dados nas entradas (Entry)
-                    if tipoDePeca in TipoPecaLista:
-                        self.TipoDePecaCB.set(tipoDePeca)
-                    if fornecedor in nome_fornecedoresLista:
-                        self.fornecedorCB.set(fornecedor)
                     self.DescricaoEntry.insert(0, descricao)
                     self.QuantidadeEntry.insert(0, quantidade)
                     self.LoteEntry.insert(0, lote)
                     self.ValorEntry.insert(0, valor)
                     self.CodigoEntry.insert(0, cod_peca)
+
+                    #Inserindo os dados nas combo box:
+                    if tipoDePeca in TipoPecaLista:
+                        self.TipoDePecaCB.set(tipoDePeca)
+                    if fornecedor in nome_fornecedoresLista:
+                        self.fornecedorCB.set(fornecedor)
             
-                    messagebox.showinfo("Success", "Produto encontrado")
+                    messagebox.showinfo("Success", "Peça encontrado")
                 else:
-                    messagebox.showwarning("Não encontrado", "Produto não encontrado")
+                    messagebox.showwarning("Não encontrado", "Peça não encontrado")
 
             except Exception as e:
                 print(f'Error: {e}') #SE EXEPT, EXIBE O ERRO (SALVOU O CODIGO)
 
 
-        #BOTAO DE PESQUISA :)
+        #BOTAO DE PESQUISA
         PesquisarButton = tk.Button(self.root,text = "Pesquisar",font= ("Georgia",10),width=13,command=pesquisar_peca)
         PesquisarButton.place(x = 20,y=390)
 
@@ -352,5 +355,5 @@ class PRODUTO:
 
 if __name__ == "__main__":
     root = tk.Tk()
-    app = PRODUTO(root)
+    app = PECA(root)
     root.mainloop()
